3dde3e87d9f5aff14110f329786bb09499ea8a80
vladimirlev <vl.vl.levchenko@gmail.com>
2018-11-25 18:56:29 +0200

    added js

---
+++ js/videos.js
@@ -0,0 +1,5 @@
+function myFunction1() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTItbGF3bm1vd2VyLWRvZy1lbg==.mp4";
+    document.getElementById("myVideo1").src = vid;
+    document.getElementById("myVideo1").controls = true;
+}
