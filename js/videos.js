6e6f937ea619c1bc7afd5b6b9701a7de1e0d59ba
vladimirlev <vl.vl.levchenko@gmail.com>
2018-11-26 03:57:47 +0200

    minor fix

--- js/videos.js
+++ js/videos.js
@@ -1,6 +1,6 @@
-function myFunction1() {
+function vid1_1() {
     var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTItbGF3bm1vd2VyLWRvZy1lbg==.mp4";
-    document.getElementById("myVideo1").src = vid;
-    document.getElementById("myVideo1").controls = true;
-    document.getElementById("myVideo1").autoplay = true;
+    document.getElementById("video1_1").src = vid;
+    document.getElementById("video1_1").controls = true;
+    document.getElementById("video1_1").autoplay = true;
 }
