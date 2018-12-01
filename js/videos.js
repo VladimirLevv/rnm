b7a61d96a34208bf2169daf84c34e857a54e28c6
vladimirlev <vl.vl.levchenko@gmail.com>
2018-12-02 01:30:19 +0200

    sneaky videos hehe

--- js/videos.js
+++ js/videos.js
@@ -1,6 +1,217 @@
 function vid1_1() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTItbGF3bm1vd2VyLWRvZy1lbg==.mp4";
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTEtcGlsb3QtZW4=.mp4";
     document.getElementById("video1_1").src = vid;
     document.getElementById("video1_1").controls = true;
     document.getElementById("video1_1").autoplay = true;
+}
+
+function vid1_2() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTItbGF3bm1vd2VyLWRvZy1lbg==.mp4";
+    document.getElementById("video1_2").src = vid;
+    document.getElementById("video1_2").controls = true;
+    document.getElementById("video1_2").autoplay = true;
+}
+
+function vid1_3() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTMtYW5hdG9teS1wYXJrLWVu.mp4";
+    document.getElementById("video1_3").src = vid;
+    document.getElementById("video1_3").controls = true;
+    document.getElementById("video1_3").autoplay = true;
+}
+
+function vid1_4() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTQtbS1uaWdodC1zaGF5bS1hbGllbnMtZW4=.mp4";
+    document.getElementById("video1_4").src = vid;
+    document.getElementById("video1_4").controls = true;
+    document.getElementById("video1_4").autoplay = true;
+}
+
+function vid1_5() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTUtbWVlc2Vla3MtYW5kLWRlc3Ryb3ktZW4=.mp4";
+    document.getElementById("video1_5").src = vid;
+    document.getElementById("video1_5").controls = true;
+    document.getElementById("video1_5").autoplay = true;
+}
+
+function vid1_6() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/Ni1lcGl6b2QtNi1yaWNrLXBvdGlvbi05LUVO.mp4";
+    document.getElementById("video1_6").src = vid;
+    document.getElementById("video1_6").controls = true;
+    document.getElementById("video1_6").autoplay = true;
+}
+
+function vid1_7() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/Ny1lcGl6b2QtNy1yYWlzaW5nLWdhem9ycGF6b3J5LUVO.mp4";
+    document.getElementById("video1_7").src = vid;
+    document.getElementById("video1_7").controls = true;
+    document.getElementById("video1_7").autoplay = true;
+}
+
+function vid1_8() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTgtcml4dHktbWludXRlcy1lbg==.mp4";
+    document.getElementById("video1_8").src = vid;
+    document.getElementById("video1_8").controls = true;
+    document.getElementById("video1_8").autoplay = true;
+}
+
+function vid1_9() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTktc29tZXRoaW5nLXJpY2tlZC10aGlzLXdheS1jb21lcy1lbg==.mp4";
+    document.getElementById("video1_9").src = vid;
+    document.getElementById("video1_9").controls = true;
+    document.getElementById("video1_9").autoplay = true;
+}
+
+function vid1_10() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTEwLWNsb3NlLXJpY2stY291bnRlcnMtb2YtdGhlLXJpY2sta2luZC1lbg==.mp4";
+    document.getElementById("video1_10").src = vid;
+    document.getElementById("video1_10").controls = true;
+    document.getElementById("video1_10").autoplay = true;
+}
+
+function vid1_11() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTExLXJpY2tzeS1idXNpbmVzcy1lbg==.mp4";
+    document.getElementById("video1_11").src = vid;
+    document.getElementById("video1_11").controls = true;
+    document.getElementById("video1_11").autoplay = true;
+}
+
+function vid2_1() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTEtYS1yaWNrbGUtaW4tdGltZS1lbg==.mp4";
+    document.getElementById("video2_1").src = vid;
+    document.getElementById("video2_1").controls = true;
+    document.getElementById("video2_1").autoplay = true;
+}
+
+function vid2_2() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTItbW9ydHluaWdodC1ydW4tZW4=.mp4";
+    document.getElementById("video2_2").src = vid;
+    document.getElementById("video2_2").controls = true;
+    document.getElementById("video2_2").autoplay = true;
+}
+
+function vid2_3() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTMtYXV0by1lcm90aWMtYXNzaW1pbGF0aW9uLWVu.mp4";
+    document.getElementById("video2_3").src = vid;
+    document.getElementById("video2_3").controls = true;
+    document.getElementById("video2_3").autoplay = true;
+}
+
+function vid2_4() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTQtdG90YWwtcmlja2FsbC1lbg==.mp4";
+    document.getElementById("video2_4").src = vid;
+    document.getElementById("video2_4").controls = true;
+    document.getElementById("video2_4").autoplay = true;
+}
+
+function vid2_5() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTUtZ2V0LXNjaHdpZnR5LWVu.mp4";
+    document.getElementById("video2_5").src = vid;
+    document.getElementById("video2_5").controls = true;
+    document.getElementById("video2_5").autoplay = true;
+}
+
+function vid2_6() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTYtdGhlLXJpY2tzLW11c3QtYmUtY3JhenktZW4=.mp4";
+    document.getElementById("video2_6").src = vid;
+    document.getElementById("video2_6").controls = true;
+    document.getElementById("video2_6").autoplay = true;
+}
+
+function vid2_7() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTctYmlnLXRyb3VibGUtaW4tbGl0dGxlLXNhbmNoZXotZW4=.mp4";
+    document.getElementById("video2_7").src = vid;
+    document.getElementById("video2_7").controls = true;
+    document.getElementById("video2_7").autoplay = true;
+}
+
+function vid2_8() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTgtaW50ZXJkaW1lbnNpb25hbC1jYWJsZS0yLXRlbXB0aW5nLWZhdGUtZW4=.mp4";
+    document.getElementById("video2_8").src = vid;
+    document.getElementById("video2_8").controls = true;
+    document.getElementById("video2_8").autoplay = true;
+}
+
+function vid2_9() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTktbG9vay13aG9zLXB1cmdpbmctbm93LWVu.mp4";
+    document.getElementById("video2_9").src = vid;
+    document.getElementById("video2_9").controls = true;
+    document.getElementById("video2_9").autoplay = true;
+}
+
+function vid2_10() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTI=/ZXBpem9kLTEwLXRoZS13ZWRkaW5nLXNxdWFuY2hlcnMtZW4=.mp4";
+    document.getElementById("video2_10").src = vid;
+    document.getElementById("video2_10").controls = true;
+    document.getElementById("video2_10").autoplay = true;
+}
+
+function vid3_1() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTEtdGhlLXJpY2tzaGFuay1yaWNrZGVtcHRpb24tZW4=.mp4";
+    document.getElementById("video3_1").src = vid;
+    document.getElementById("video3_1").controls = true;
+    document.getElementById("video3_1").autoplay = true;
+}
+
+function vid3_2() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTItcmlja21hbmNpbmctdGhlLXN0b25lLWVu.mp4";
+    document.getElementById("video3_2").src = vid;
+    document.getElementById("video3_2").controls = true;
+    document.getElementById("video3_2").autoplay = true;
+}
+
+function vid3_3() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTMtcGlja2xlLXJpY2stZW4=.mp4";
+    document.getElementById("video3_3").src = vid;
+    document.getElementById("video3_3").controls = true;
+    document.getElementById("video3_3").autoplay = true;
+}
+
+function vid3_4() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTQtdmluZGljYXRvcnMtMy10aGUtcmV0dXJuLW9mLXdvcmxkZW5kZXItZW4=.mp4";
+    document.getElementById("video3_4").src = vid;
+    document.getElementById("video3_4").controls = true;
+    document.getElementById("video3_4").autoplay = true;
+}
+
+function vid3_5() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTUtdGhlLXdoaXJseS1kaXJseS1jb25zcGlyYWN5LWVu.mp4";
+    document.getElementById("video3_5").src = vid;
+    document.getElementById("video3_5").controls = true;
+    document.getElementById("video3_5").autoplay = true;
+}
+
+function vid3_6() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTYtcmVzdC1hbmQtcmlja2xheGF0aW9uLWVu.mp4";
+    document.getElementById("video3_6").src = vid;
+    document.getElementById("video3_6").controls = true;
+    document.getElementById("video3_6").autoplay = true;
+}
+
+function vid3_7() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTctdGhlLXJpY2tsYW50aXMtbWl4dXAtZW4=.mp4";
+    document.getElementById("video3_7").src = vid;
+    document.getElementById("video3_7").controls = true;
+    document.getElementById("video3_7").autoplay = true;
+}
+
+function vid3_8() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTgtbW9ydHlzLW1pbmQtYmxvd2Vycy1lbg==.mp4";
+    document.getElementById("video3_8").src = vid;
+    document.getElementById("video3_8").controls = true;
+    document.getElementById("video3_8").autoplay = true;
+}
+
+function vid3_9() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTktdGhlLWFiY3Mtb2YtYmV0aC1lbg==.mp4";
+    document.getElementById("video3_9").src = vid;
+    document.getElementById("video3_9").controls = true;
+    document.getElementById("video3_9").autoplay = true;
+}
+
+
+function vid3_10() {
+    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTM=/ZXBpem9kLTEwLXRoZS1yaWNrY2h1cmlhbi1tb3J0eWRhdGUtZW4=.mp4";
+    document.getElementById("video3_10").src = vid;
+    document.getElementById("video3_10").controls = true;
+    document.getElementById("video3_10").autoplay = true;
 }
