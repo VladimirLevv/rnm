8051f451bf71511b9172819e29152ed3fa5e0484
vldmr_lev <vl.vl.levchenko@gmail.com>
2019-05-14 21:09:25 +0300

    first season vds resolved

--- js/videos.js
+++ js/videos.js
@@ -1,75 +1,75 @@
 function vid1_1() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTEtcGlsb3QtZW4=.mp4";
+    var vid = "https://ohmanyfast.verystream.net/stream/BGkyQcEhggi/rogugbt3sYNCArCA/wEGU-QEgdAa5ZCKKaiqfGeeklw8v_mFl_mEyUkpvZFGu4mkyETxmrEV8Kv588AcaNQGvfHpiJshmSi-RhS1Mn9Locp64Y2A6ZaneVhxhc2YUYOY9aK-QgdVfhG73o4THrt4J7Q2fOHvuL5szRZQA8OigGsA91n93qlrRnHy0xLLCKHg3itKX58ymaAK1z6UN2ChwxuUZzetSb8lGiJA9jhJ3bRAxuq9oOLd0vvD-I5ia2qhgPmmSrpQqAictLWsymfeGThS0_b7JZ_SdLCVKA06wBTzrHK5sFarThQAZ3Jax0xSHM_dbkDvUHFmzCMPunYh-fe04SSNuY_DJR_4mbHvReWM4-i2vKlI8l8Nz70Q4oDqCy9R7aH2bqUyXYISUFaNQmoDSUoPovi4IXRXN7KMswKW5JdwWteyfpt2PzWjOOzt7Lt3ULwbIU6JiSXt0OC5C-WfjWzVFpCJfJdBc_A/Watch+RIck+And+Morty.S01E01.720p.BRRip.AC3.5.1.x264+.mp4";
     document.getElementById("video1_1").src = vid;
     document.getElementById("video1_1").controls = true;
     document.getElementById("video1_1").autoplay = true;
 }
 
 function vid1_2() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTItbGF3bm1vd2VyLWRvZy1lbg==.mp4";
+    var vid = "https://suchquickwow.verystream.net/stream/5PMzMMUJuVn/2Gn1X-Oy3tgANmZ-/w86_-P2JPpQHcPexglxcSaE_MKBE7eemUr8yw2nmT-izYT_HrAZstw3K7KVGin7uAANrEFLjXe1He-O_3u_NmIvrxBlcMMmrHPU81dAK_vutgrRm4OHqlIzgVd3YMUdvqbuI09nEYxCYCI0piLgc-2bGDWEq7JBTbHdX8uJdv45LmJTPif6ye_3KPy1Xhseh8Ad-uoamHv5ijQy3DbaEZmOWvRq5skmVC5PIRu1lryJ0Wj9qEge9quVI2Yo7hAElhUl15s9J7RU8PqpKlq5-7JlqbjeNuWexjW3KmC_gUl14_fyKTF8PK4Ulbdt01hNmb-LEg_Y47f8WIQZh2Co9uDz7Flt6a55dngSYdgoLcEKa38qyXb5Nf4uMFh_jxtHpktL8_FVyujIDBSCALwDBMjN5z3d7JGMPxMR45pWp7VAeLUqypekekrNMf8BdiZ_ntRRt-sPXxL_wbLGIoqH_EGDa_5gECVgDTHMwUA8dPgU/Watch+%5Bst%5DRick_and_Morty_S01E02%5Bx264-10bit%5D%5BAC3%5D%5B1080p%5D%5BBDrip%5D.mp4";
     document.getElementById("video1_2").src = vid;
     document.getElementById("video1_2").controls = true;
     document.getElementById("video1_2").autoplay = true;
 }
 
 function vid1_3() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTMtYW5hdG9teS1wYXJrLWVu.mp4";
+    var vid = "https://ohveryrapid.verystream.net/stream/Akdhd9Zr6gr/Q1FHa_ybJk4HiXbm/sZKkL15otA2U-Dr-t0FSMqt9TV4vBITCbYEopOoNIJbgTE4l13KzELYyIBASHR1b825_C-qA1Lwr91fVQGuuOUHw1vLxGmcgy_LwtRhPlkIswq03sI3sZ_WYHzEivg0POfwFPMbbu2XvNRne4vg7c-KN2LW2jut2yocldJGXey2eFo93HJdGQhvZiCzLyDLWSlqQf-mwxGS5ag7vs5GPcMuVOGce1DoPH2XIQUJQlQWOrBSctXnVm3TgnsDfaEfuNoicMuMszZyCqQ-CbOxShLMO2A8o07rVt8SkdMuZfAyDMrP7fl8cdTxO1jMeOeDY5oCcIcVQ4xIPZUklWNnWegM2jCFh5NYkQ4eMu-Dla-wBYR6xONMjyE-XbAqszSuU_8YkvnGwtO0Ma8jiYli-alXU53psDIH4HuIy_NfxpqTVoQszPlWEcONXXUNDkMcBIptq0JMi0qg9JU-QVyc4zIdMkcZZW_CVgsxqTMXL4C9QGMqPdfHK_zSbpdwyUvb46EJwnVnRzMhkmtSfUnsrqA/Watch+%5Bpseudo%5D+Rick+And+Morty+S01E03+Season+1+Episode+3+Anatomy+Park+%5BBDRip%5D+%5B1080p%5D+%5Bh.265%5D.mp4";
     document.getElementById("video1_3").src = vid;
     document.getElementById("video1_3").controls = true;
     document.getElementById("video1_3").autoplay = true;
 }
 
 function vid1_4() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTQtbS1uaWdodC1zaGF5bS1hbGllbnMtZW4=.mp4";
+    var vid = "https://goodamaze.verystream.net/stream/Qjn43Nk8iya/D6SpoLxnmYFsJ6Yc/zN6BVgeW19WHKygFip809fjNImZpzC46X1IfatFQBgfDgX1DD8p_UEszxlEx83pc3BzQZVWNqCq4HRZLKJPh4zjAJIujE9c-8KsLGbQXDBtQHvxaApmogCer1nTe9udLqzow8XTjR3bBgOiY4oO_6czKaqDXrND7Y6_3F6jMdQFrv1KKIX8iy7dZrqjEmADd5PWZeRMP359DZSs0zaEzxlQTsrRlWCPtcWrSYSyK4qkiYs_D3T2V9kDriTO__AVmpTQ2R0JCwYxc1ng8e9si3_7tMLDmwTTh_qmDwNw6GCGUUaeyh7UNFX1t_pmP7dFgPWeXubOQC8Y7vnO_ZwTOOT4YmQlilHVeVjapu7-o3hFKjaF2YpLvhmAAK_7ECiPGYleZz7uNIUzv9zDRoIXkAQCEf3SqbB0lNYSM2hiC_GXNnd9gPLbPpByOh6Xbz8ocgNHCUQnENIZ5XaS-8IpcoA/Watch+%5Bpseudo%5D+Rick+And+Morty+S01E04+Season+1+Episode+4.mp4";
     document.getElementById("video1_4").src = vid;
     document.getElementById("video1_4").controls = true;
     document.getElementById("video1_4").autoplay = true;
 }
 
 function vid1_5() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTUtbWVlc2Vla3MtYW5kLWRlc3Ryb3ktZW4=.mp4";
+    var vid = "https://sofrontendwow.verystream.net/stream/XMuVs5ayVYP/8u3ZKCWn32MECIkX/VQXBs_1kDmhdysbluQFQQyYWex-Av9P11L_hvuZWB_22QPu88MdvNEunzo4Ved82pRxDI8LD-GNHbUef1B3W_fTEjJ3Ra3RqcIra5qktJcuP-1uweSmYBejX31ZrkYaM1IW_7o-P3RJu6Fe-9nGfVkBJY6t70P6gedEvjSxIOEgiwqnCgnux_JRIwQNc44--WNxIgo9FQOvnZ0YSFAmeLURgn6Ed59oNKF5m7-TLkiHhKct_r_uk4ZiGVr-FnBSaeLXaDxCO18hkWyjLDqCEjNo02ukSgE2HVPcV32Dv3wvp1vxUIfzxZEISnvqf4daJxfwR81Zq4boSw-ceTHJeW-w1saES41CW-4rCnULh3FTslx9PiyT99aLints3KyCLi5AhZwb-QNCKAo5-vOqzCbJuCiOXc9ijge1jJA4x9G_-GWZVuD9RFoNkZuwPPLkrlrcSwXVtTb9rPKBjwfRz9Tj1EEzNwiUtNYb084elyepxj2VomVlDKyx0HS5r7NOfR5Me5VoJM6NXTPobRMplDQ/Watch+%5Bpseudo%5D+Rick+And+Morty+S01E05+Season+1+Episode+5+Meeseeks+And+Destroy+%5BBDRip%5D+%5B1080p%5D.mp4";
     document.getElementById("video1_5").src = vid;
     document.getElementById("video1_5").controls = true;
     document.getElementById("video1_5").autoplay = true;
 }
 
 function vid1_6() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/Ni1lcGl6b2QtNi1yaWNrLXBvdGlvbi05LUVO.mp4";
+    var vid = "https://moredatapls.verystream.net/stream/isuDvcHz17u/w3C8oo58AI63ok73/QcIClQl2Ejs6eVb3hO9YTEX0lZiY9NVKrFI8VICG98pVuQp66APniXp2Z4dYou2zqtNTUYk4hrXa4wibQycua7JwIbv-dsTdLLdia0nKNIPAX6RTmcWI4JHD9MileBcsFaqrKuSZYNuk-785NkWN30geuIWxNctRZ7CsfJMtnr0zG0fz7DhvcLh50ThpfrnExY58OV_h186XjIJVmMczKDTBYM1wV0vmSFaTaSX6XK13SNfQ8Z9JfKzo5sFoiyR3vrn5evjPglcajqltAAVgiYI2_rAlFFrrswx6yi33PNs8QxjIVcJ7vNhoQyyidrsN6L9KKgRCtR0btvAHgrDMl5ICL0vj3T-9-VJG4CQCD9fXQvesy1-R5RX-oDT_PMxuhrIwbWNDihnNwXTDnypaVsydRCZ_MvLIhdZN2YOiBMb2cIQMeUbLgqWZKj3WsAfs2UXchfCJVrd4rk0Ql0OVu5NQTm2bzxB7sbqpDWlrS-932-BEHpx2c4FljqH5ka8d/Watch+%5Bpseudo%5D+Rick+And+Morty+S01E06+Season+1+Episode+6+Rick+Potion+%239+%5BBDRip%5D+%5B1080p%5D.mp4";
     document.getElementById("video1_6").src = vid;
     document.getElementById("video1_6").controls = true;
     document.getElementById("video1_6").autoplay = true;
 }
 
 function vid1_7() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/Ny1lcGl6b2QtNy1yYWlzaW5nLWdhem9ycGF6b3J5LUVO.mp4";
+    var vid = "https://wowmanyscare.verystream.net/stream/ivZyD5iuw7k/TkR_886GRSztfI52/GwMkqDmWAIOL8vGIPtbPa8qPFMh9BeDfxcAhj3NQhEbJ5g8KlldZTCKnxmzcpv3ErSV09n-jRpQpmqx2SZi6_CXqYmgMMb77MLeTEr4OboOraaIfc79pOZmck6MTaoGho1_9XJcp6j7dpyFs7sJ_sz3Qn2OSpWuhKTXiTVhdZxDpi0aWse491WBC3xO8WdMi3bqCfI7n4wrfwR79Za26Dlo4jm1l7X2lizSsLiloZpHSScUXJCTKBQn3wVtXtJSGoQKARLLTD_9cOzRbXWUgOhc6sNOqBHZtQO8qyErlEIzItxqH6YMaOcDaoyglUjI6lG_mjM9MAnxraMbGyM_cWwNlhP4VQc1Kp_UqXFHlPQvKOdR2JjoC_ZRXZkA9wlWhsnXQukXT7jJpFSi3Y3khzxfdTB0GNZ243XNaA5doButWkXul7GwZfBeFcYdFYv7gpCwEttmUNVFXAYR8xl1C1eiuJ_iGx9_9zPuVQrv-LWqAtlR7R8wH-_lsCXiryBnjoJNDSarqfMKiDx9ElqlUIw/Watch+%5Bpseudo%5D+Rick+And+Morty+S01E07+Season+1+Episode+7+Raising+Gazorpazorp+%5BBDRip%5D+%5B1080p%5D.mp4";
     document.getElementById("video1_7").src = vid;
     document.getElementById("video1_7").controls = true;
     document.getElementById("video1_7").autoplay = true;
 }
 
 function vid1_8() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTgtcml4dHktbWludXRlcy1lbg==.mp4";
+    var vid = "https://ohsuchfrontend.verystream.net/stream/jCWnSuoRd8e/ZcKtEI3IyB6O2FAO/z_mPZgSj7b4NiWy6SMJUgnNytKn93xLkD4JdutPHjYzaW1p_FUV4Uuinn6GN3Mgk-otOt2Uc8M5H5bPOGssO5v5mISmvnp4w6ndzLmNFBp1nOpPXqzs477NxjBgM3XT39Hz1ynH2MlUY-ctIp8aHpZA91xmCGDTasMuqVqjVFPGhRl2Z9Tn2tqB7NEH9-fk6x3ExrfUwF6LYWYZhoMdJ_frhaFJ-haeWFVOXHOh6t42PWWpJEV6zAIaaXaTe8nRtvaMrw5Ql66rnX7O-IfmrUlICX609wmZW1x0ibsuROa3_s3Xj6-9R2izAWHxBthM3iLppv1ZAUE3j2QW3rQusLZK9sgw4nb8YioYOLxGLcwWvutLNgW3hIcYfOXn2fc1JpwjS8ofnmfP-e-5X_V5ZuRWbI3WBoiesmZwXDr9-RYli7GNejdz1q3oBpPThTGZEbdgP90PHNJJnmBrCytKt2JJeVjquKk1JmjBY_Lw83tp8AdBAkaHQPMxgyrs8Yn12/Watch+%5Bpseudo%5D+Rick+And+Morty+S01E08+Season+1+Episode+8+Rixty+Minutes+%5BBDRip%5D+%5B1080p%5D.mp4";
     document.getElementById("video1_8").src = vid;
     document.getElementById("video1_8").controls = true;
     document.getElementById("video1_8").autoplay = true;
 }
 
 function vid1_9() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTktc29tZXRoaW5nLXJpY2tlZC10aGlzLXdheS1jb21lcy1lbg==.mp4";
+    var vid = "https://ohmuchrapidwow.verystream.net/stream/6vTXBwhLKqf/QYyBGGFfX2yE0aUO/p65SPyXDYJbT6KHIJ8kVhm85rN4W4HzifI9A1nub7Klf0jFh8xkLJPabO1EmFPU54R8KsAAi0164piD0w_KYEbFTuR69kW1CfR4XLfwXBYz23veEvFkDjf2apJO6JnLT1apLieifd7XjtbyQ1UBrDWvW4Rf3ZkDCGek_mLoGSHJSPl85X0yYfAL8yI2FLaRdXKkdU8TRKM5_5QGDHKRJ2X1kTsLtxAyBfHQrsYT4s1pg-nzmCvDqHspG6HgdPPflzWLsOdUCnCQo7_iJkK6eCQTL8UyxuJArgGV4zVVHnSn_P3coN72uGArbpBSJhW4w6xKjzmrhPdnUo9GUUSJmi7V-dK4VZw5gEQS6645BoFbOxIc1aXm-0YGp3b8XrvzMybCEWR7CXQ8E0w2IfeeAuC0x9HhKdyRDBr7d2WWOj8lZfsi2Ef7f4ANiVliIaZIHP8ZZtW8vleO6KtO0ATh14E_5Mz_1SGeBnDGFm_JJonFiqj1Sm7Rta2BfEsTkiZj8gWoktkoqURCz3k0zIR6FKrYm26TO_5BkgztkQGDzkOQ/Watch+%5Bpseudo%5D+Rick+And+Morty+S01E09+Season+1+Episode+9+Something+Ricked+This+Way+Comes+%5BBDRip%5D+%5B1080p%5D+%5Bh.265%5D.mp4";
     document.getElementById("video1_9").src = vid;
     document.getElementById("video1_9").controls = true;
     document.getElementById("video1_9").autoplay = true;
 }
 
 function vid1_10() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTEwLWNsb3NlLXJpY2stY291bnRlcnMtb2YtdGhlLXJpY2sta2luZC1lbg==.mp4";
+    var vid = "https://suchserverwow.verystream.net/stream/Sgz75WwURYJ/r78C9SNXTretrtky/UD3MP--nkywSYSxVRp9_JTlobYRedk1DegmII26_OQ8JHagvQ00ARneWPTzzBGrB3Lm6rwRSfNbHMSSyklb1Oiyo8m7tw27BDbo0mMTX2iGJkmss4tF0DZr5iVNa_a9ToQ-0ZWvfdC8jwQiY-4SMiyvbbvTanPsHPwcLMyQTzJC2ABb-oTfgafMmhVc_PgM9T4PUrYqwg5k-DfuaNvTUWsuI5MybevcjsS6FdR8gxxHiF9JMhRaPV3eB9JUPsPirZirHbkxKR5FTtZOZqAYqQNaxQ39Rv8SaSj2RfjRFzOgWs5xsSI_Gc21yq6oZEVafI5ZzlwHEPErlPCHagtKGzpBygDRQFbCj1Pn3TnDhYoFI8Lm3vjg9LaRBPToR51Cdxtz_qLtkjX55upEHCIpWes97wJwiAVO4Kg46zLDlqiYfnLMDKKY74djSU7Swl9Q8GKfevivKGJCwr5okIU8PtUHA290pO2NAEYP912f0y_U0tclJDvbWA56WgXuGmnEEyCXTfIHXPAZYoVipj50JftaG4NXomc2l_cZxZ97CWVc/Watch+%5Bpseudo%5D+Rick+And+Morty+S01E10+Season+1+Episode+10+Close+Rick-counters+Of+The+Rick+Kind+%5BBDRip%5D+%5B1080p%5D.mp4";
     document.getElementById("video1_10").src = vid;
     document.getElementById("video1_10").controls = true;
     document.getElementById("video1_10").autoplay = true;
 }
 
 function vid1_11() {
-    var vid = "https://pixa.club/dmlkZW8=/cmljay1hbmQtbW9ydHk=/c2Vhc29uLTE=/ZXBpem9kLTExLXJpY2tzeS1idXNpbmVzcy1lbg==.mp4";
+    var vid = "https://www1047.hlsmp4.com/token=eRE_ezeTIX6OY5rDBOUyyQ/1557871698/176.9.0.0/34/6/03/ff8d7e7a0d60c0f0a0c073e744536036-480p.mp4";
     document.getElementById("video1_11").src = vid;
     document.getElementById("video1_11").controls = true;
     document.getElementById("video1_11").autoplay = true;
